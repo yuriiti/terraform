3a8f3b6ab6ce6805693e386839958a62896284cb
Yuri <hannyfeo@gmail.com>
2025-04-24 12:44:08 +0200

    Refactor TypeScript configuration; adjust include paths and clean up types

--- apps/app/src/main.ts
+++ apps/app/src/main.ts
@@ -8,13 +8,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-    process.env.TF_VAR_endpoint_url
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
   await app.listen(port);
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`);
+  Logger.log(
+    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+  );
 }
 
 bootstrap();
