7d17bc44eec0fc58c0871a2374b2bbab339f1ac8
Yuri <hannyfeo@gmail.com>
2025-04-25 15:05:52 +0200

    Refactor s3-lambda configuration; update build executor to webpack, enhance output settings, and add zip-webpack-plugin for packaging

---
+++ apps/s3-lambda/webpack.config.js
@@ -0,0 +1,21 @@
+const { composePlugins, withNx } = require('@nx/webpack');
+const ZipPlugin = require('zip-webpack-plugin');
+
+// Nx plugins for webpack.
+module.exports = composePlugins(withNx(), (config) => {
+  // Note: This was added by an Nx migration. Webpack builds are required to have a corresponding Webpack config file.
+  // See: https://nx.dev/recipes/webpack/webpack-config-setup
+  config.devtool = 'source-map';
+  config.output.filename = 'main.js';
+
+  config.plugins.push(
+    new ZipPlugin({
+      filename: 's3-lambda.zip',
+      path: '../..',
+      pathPrefix: '',
+      include: [/\.js$/, /\.json$/],
+    })
+  );
+
+  return config;
+});
